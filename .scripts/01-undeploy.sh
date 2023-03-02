bd552d8e23d12abd1e923e34bdf7b0b58ee15319
souhmaa_ <smaalaoui@gopartner.fr>
2023-03-02 02:43:04 +0100

    Enable build / test stages

--- .scripts/01-undeploy.sh
+++ .scripts/01-undeploy.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-process=`ps -ef | grep -v awk | awk -e '/java.*app.war/ { print $2 }'`
-echo $process
-
-
-if [ -n "$process" ]; then
-        kill -9 $process
-fi
-
+sudo systemctl stop app.service
 if test -f "/home/ubuntu/app/app.war"; then
     rm -rf /home/ubuntu/app
 fi
--- .scripts/02-deploy.sh
+++ .scripts/02-deploy.sh
@@ -3,4 +3,6 @@
 mkdir -p /home/ubuntu/app
 mv /home/ubuntu/app.war /home/ubuntu/app/app.war
 
-nohup java -jar /home/ubuntu/app/app.war > output.log &
+sudo systemctl start app.service
+
+sleep 20
